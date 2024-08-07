6e04aabc8c13523e4d55ab95ea0350fe3d30a20f
mgallais <mgallais@localhost.localdomain>
2024-08-07 11:43:48 +0200

    Ahoy~ !

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,4 +1,8 @@
-service mysql start;
+#!/bin/bash
+
+# Start MySQL service and log output
+service mysql start || { echo 'MySQL start failed'; cat /var/log/mysql/error.log; exit 1; }
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\';"
 mysql -e "CREATE USER IF NOT EXISTS \'${SQL_USER}\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
